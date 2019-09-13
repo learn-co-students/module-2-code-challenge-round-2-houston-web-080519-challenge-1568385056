82c8116d9710a155bf169b8fc16a4edd9b0c9e73
resham <resham.arora@gmail.com>
2019-09-13 11:23:58 -0500

    0

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,5 @@
 class Episode < ApplicationRecord
+
+    has_many :guest
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get('/guests', to: 'guests#index')
   get('/episodes', to: 'episodes#index')
+  get('/episodes/:id', to: 'episodes#show')
+  get('/appearances', to: 'appearances#index')
+  get('/appearances/new', to:'appearances#new')
+  post('/createappearance', to:'appearances#create')
 end
